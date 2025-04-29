1a873669a5bd49b0420e97d9a00140aceedd51c0
JoeyDinatale2 <jdin0054@student.monash.edu>
2025-04-29 13:02:58 +1000

    Create audioreceive.py

---
+++ audioreceive.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+import wave
+import serial
+import serial.tools.list_ports
+
+BAUD_RATE = 115200
+SAMPLE_RATE = 10000
+
+# Select the first com port
+ports = serial.tools.list_ports.comports()
+device = None
+for i in ports:
+    if i.description[0:3] == "STM":
+        device = i
+
+print(f"STM Serial Port = {device.device}")
+
+ser = serial.Serial(device.device, BAUD_RATE)
+
+data = []
+
+for i in range(SAMPLE_RATE*5):
+    ch = ser.read(1)
+    data.append(int.from_bytes(ch))
+
+
+data = np.array(data)
+
+data = (data - data.min()) / data.max()
+data = data * 255
+
+data = data.astype(np.uint8)
+
+with wave.open("output.wav", 'wb') as wave_file:    
+    wave_file.setnchannels(1)
+    wave_file.setsampwidth(1)
+    wave_file.setframerate(SAMPLE_RATE)
+    wave_file.writeframes(data.tobytes())
